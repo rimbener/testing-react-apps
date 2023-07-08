4d508dbd4beafe40c558e88e03a69ff348b3e898
Hernán Laura <hernan.laura@openenglish.com>
2023-07-08 15:02:46 -0300

    Exercise 4 extra credit 2 complete

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -2,14 +2,20 @@
 // http://localhost:3000/login
 
 import * as React from 'react'
+import faker from 'faker'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Login from '../../components/login'
 
+function buildLoginForm() {
+  return {
+    username: faker.internet.userName(),
+    password: faker.internet.password(),
+  }
+}
 test('submitting the form calls onSubmit with username and password', async () => {
   const user = userEvent.setup()
-  const username = 'Some User Name'
-  const password = 'secretPassword'
+  const {username, password} = buildLoginForm()
 
   const mockOnSubmit = jest.fn()
   render(<Login onSubmit={mockOnSubmit} />)
