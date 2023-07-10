41446c1540a6a473e4c6111692723daee090b927
Hernán Laura <hernan.laura@openenglish.com>
2023-07-10 17:21:36 -0300

    Exercise 5 extra 1 complete

--- src/__tests__/exercise/05.js
+++ src/__tests__/exercise/05.js
@@ -1,13 +1,12 @@
 // mocking HTTP requests
 // http://localhost:3000/login-submission
 
-import * as React from 'react'
+import {build, fake} from '@jackfranklin/test-data-bot'
 import {render, screen, waitForElementToBeRemoved} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import {build, fake} from '@jackfranklin/test-data-bot'
-import {rest} from 'msw'
 import {setupServer} from 'msw/node'
 import Login from '../../components/login-submission'
+import {handlers} from '../../test/server-handlers'
 
 const buildLoginForm = build({
   fields: {
@@ -16,24 +15,7 @@ const buildLoginForm = build({
   },
 })
 
-const server = setupServer(
-  rest.post(
-    'https://auth-provider.example.com/api/login',
-    async (req, res, ctx) => {
-      if (!req.body.password) {
-        return res(ctx.status(400), ctx.json({message: 'password required'}))
-      }
-      if (!req.body.username) {
-        return res(ctx.status(400), ctx.json({message: 'username required'}))
-      }      
-      return res(
-        ctx.json({
-          username: req.body.username,
-        }),
-      )
-    },
-  ),
-)
+const server = setupServer(...handlers)
 
 beforeAll(() => server.listen())
 afterAll(() => server.close())
