bd221bcf11b7c8c7dd75b29dadb681f0a4c9cb69
Hernán Laura <hernan.laura@openenglish.com>
2023-07-12 14:44:08 -0300

    Exercise 8 extra 2 complete

--- src/__tests__/exercise/08.js
+++ src/__tests__/exercise/08.js
@@ -4,6 +4,15 @@
 import {act, fireEvent, render, screen} from '@testing-library/react'
 import useCounter from '../../components/use-counter'
 
+function setup({initialProps} = {}) {
+  const result = {}
+  function TestComponent() {
+    result.current = useCounter(initialProps)
+    return null
+  }
+  render(<TestComponent />)
+  return result
+}
 function UseCounterHookExample() {
   const {count, increment, decrement} = useCounter()
 
@@ -32,16 +41,30 @@ test('exposes the count and increment/decrement functions', () => {
 })
 
 test('exposes the count and increment/decrement functions', () => {
-  let result
-  function TestComponent(props) {
-    result = useCounter(props)
-    return null
-  }
-  render(<TestComponent />)
+  const result = setup()
+  expect(result.current.count).toBe(0)
+  act(() => result.current.increment())
+  expect(result.current.count).toBe(1)
+  act(() => result.current.decrement())
+  expect(result.current.count).toBe(0)
+})
 
-  expect(result.count).toBe(0)
-  act(() => result.increment())
-  expect(result.count).toBe(1)
-  act(() => result.decrement())
-  expect(result.count).toBe(0)
+test('allows customization of the initial count', () => {
+  const result = setup({initialProps: {initialCount: 3}})
+
+  expect(result.current.count).toBe(3)
+  act(() => result.current.increment())
+  expect(result.current.count).toBe(4)
+  act(() => result.current.decrement())
+  expect(result.current.count).toBe(3)
+})
+
+test('exposes the count and increment/decrement functions', () => {
+  const result = setup({initialProps: {step: 2}})
+
+  expect(result.current.count).toBe(0)
+  act(() => result.current.increment())
+  expect(result.current.count).toBe(2)
+  act(() => result.current.decrement())
+  expect(result.current.count).toBe(0)
 })
