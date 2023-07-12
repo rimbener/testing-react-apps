76399501ea2a6dc1d07c343b81907dab40b651ff
Hernán Laura <hernan.laura@openenglish.com>
2023-07-12 10:48:19 -0300

    Exercise 7 extra 1complete

--- src/__tests__/exercise/07.js
+++ src/__tests__/exercise/07.js
@@ -6,13 +6,28 @@ import {render, screen} from '@testing-library/react'
 import {ThemeProvider} from '../../components/theme'
 import EasyButton from '../../components/easy-button'
 
-const Wrapper = ({children}) => <ThemeProvider>{children}</ThemeProvider>
+test('renders with the default light styles for the light theme', () => {
+  const Wrapper = ({children}) => <ThemeProvider>{children}</ThemeProvider>
 
-test('renders with the light styles for the light theme', () => {
   render(<EasyButton>Easy</EasyButton>, {wrapper: Wrapper})
   const button = screen.getByRole('button', {name: /easy/i})
+
   expect(button).toHaveStyle(`
     background-color: white;
     color: black;
+  `)
+})
+
+test('renders with the dark styles for the dark theme', () => {
+  const Wrapper = ({children}) => (
+    <ThemeProvider initialTheme="dark">{children}</ThemeProvider>
+  )
+
+  render(<EasyButton>Easy</EasyButton>, {wrapper: Wrapper})
+  const button = screen.getByRole('button', {name: /easy/i})
+
+  expect(button).toHaveStyle(`
+    background-color: black;
+    color: white;
   `)
 })
